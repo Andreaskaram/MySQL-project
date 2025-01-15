95b83695f58d86c9ed3a748abbdc6007c14e0c45
ArcaneTrixter99 <apostoliszk@gmail.com>
2025-01-15 23:37:07 +0200

    Update dbproj.sql

--- dbproj.sql
+++ dbproj.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.40, for macos14 (arm64)
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
 --
 -- Host: localhost    Database: projectdb
 -- ------------------------------------------------------
--- Server version	9.1.0
+-- Server version	8.0.40
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -495,8 +495,54 @@ CREATE TABLE `venues` (
 
 LOCK TABLES `venues` WRITE;
 /*!40000 ALTER TABLE `venues` DISABLE KEYS */;
+INSERT INTO `venues` VALUES (1,'Athens Concert Hall','Athens, Greece',2000,350,'2005-06-15'),(2,'Thessaloniki Arena','Thessaloniki, Greece',10000,1200,'2010-09-10'),(3,'Patras Cultural Center','Patras, Greece',1500,450,'2000-03-25'),(4,'Heraklion Music Hall','Heraklion, Greece',3000,250,'2015-11-01'),(5,'Ioannina Open Theater','Ioannina, Greece',5000,800,'2018-05-20'),(6,'Larissa Exhibition Center','Larissa, Greece',4000,600,'2012-04-12'),(7,'Chania Concert Space','Chania, Greece',2500,150,'2020-01-01'),(8,'Kalamata Outdoor Stage','Kalamata, Greece',1000,50,'2017-07-15'),(9,'Rhodes Grand Theater','Rhodes, Greece',3500,700,'2008-10-05'),(10,'Corfu Philharmonic Hall','Corfu, Greece',1200,100,'1995-05-30');
 /*!40000 ALTER TABLE `venues` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'projectdb'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `CalculateVenueScore` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `CalculateVenueScore`(IN venueId INT, OUT venueScore INT)
+BEGIN
+    DECLARE capacityScore INT;
+    DECLARE concertsScore INT;
+    DECLARE yearsScore INT;
+    DECLARE yearsOfOperation INT;
+
+
+    SELECT 
+        Capacity, 
+        ConcertsHeld, 
+        TIMESTAMPDIFF(YEAR, DateOpened, CURDATE()) AS YearsOfOperation
+    INTO 
+        @venueCapacity, 
+        @venueConcerts, 
+        yearsOfOperation
+    FROM 
+        venues 
+    WHERE 
+        VenId = venueId;
+
+    SET capacityScore = FLOOR(@venueCapacity / 1000) * 1;
+    SET concertsScore = FLOOR(@venueConcerts / 100) * 3;
+    SET yearsScore = yearsOfOperation * 2;
+    SET venueScore = capacityScore + concertsScore + yearsScore;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -507,4 +553,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-01-15 21:59:50
+-- Dump completed on 2025-01-15 23:36:46
